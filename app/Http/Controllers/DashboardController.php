07dc7c8f54afd6dff51b0608baca2fb12975212c
inxkrutik <krutik.patel@inexture.in>
2016-11-24 10:06:11 +0530

    change

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use Storage;
 class DashboardController extends Controller {
 
     public function index() {
-        $sourceFile = "D:\Projects\wamp\www\laraveldemo51\public\images\s3.jpg";
+        $sourceFile = "http://23.22.101.111:89/images/s3.jpg";
         $targetFile = time() . ".jpg";
         $disk = Storage::disk('s3');
         $return = $disk->put($targetFile, fopen($sourceFile, 'r+'));
