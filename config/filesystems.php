1911258ec1dec63cb86ecf6804f977c25387733f
inxkrutik <krutik.patel@inexture.in>
2016-11-24 10:28:49 +0530

    change

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key'    => 'AKIAIAEKX2SUXKWIMKRQ',
+            'key'    => 'AKIAJLKIZ5JDYXJ4ECRA',
             'secret' => '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',
             'region' => 'us-east-1',
             'bucket' => 'demokrutik',
