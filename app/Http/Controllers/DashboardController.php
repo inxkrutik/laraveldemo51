a0e80a006988ae59a5253869bcacff2fea2ab9b2
inxkrutik <krutik.patel@inexture.in>
2016-11-24 11:58:05 +0530

    sample1.flv

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use Storage;
 class DashboardController extends Controller {
 
     public function index() {
-        $sourceFile = public_path("images/sample.flv");
+        $sourceFile = public_path("images/sample1.flv");
         $targetFile = time() . ".flv";
         $disk = Storage::disk('s3');
         $return = $disk->put($targetFile, fopen($sourceFile, 'r+'));
