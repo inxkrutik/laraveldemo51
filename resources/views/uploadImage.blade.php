6e1c83630230d5db921e132977412ea4a6e465fb
inxkrutik <krutik.patel@inexture.in>
2016-11-29 14:02:55 +0530

    View

---
+++ resources/views/uploadImage.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Upload File 5</div>
+                <div>
+                    <form action="uploadimage" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="file" name="fileimage" id="fileimage" accept="image/*">                        
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
