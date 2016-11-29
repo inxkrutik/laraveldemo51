a7eadb2ba11e11ad1734a99fbdedcc9880de2729
inxkrutik <krutik.patel@inexture.in>
2016-11-29 14:11:30 +0530

    de

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
             'driver' => 's3',
             'key'    => 'AKIAJXJG5KSP2R5XIBRQ',
             'secret' => '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',
-            'region' => 'Ireland',
+            'region' => 'us-east-1',
             'bucket' => 'mye3-files',
         ],
 
